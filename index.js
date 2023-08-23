4441ad20bb2dc1a11792f50e966c54437d4c7afb
Youlink <younes.rahmani@hotmail.fr>
2023-08-23 12:46:54 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("fichier chargé");
+const submit = document.querySelector(".submit");
+console.log(submit);
+submit.addEventListener('click', () => {
+    console.log("Click!");
+});
+
+const rate = document.querySelector(".rate");
+console.log(rate);
+rate.addEventListener('click', () => {
+    console.log("Clickééééé");
+});
